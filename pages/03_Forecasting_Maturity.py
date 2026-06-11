9ee877aa0b7c9ca87f1c4a34415c95909b6effd3
Hharry99 <113513693+Hharry99@users.noreply.github.com>
2026-06-11 15:26:26 +0300

    Update 03_Forecasting_Maturity.py

--- pages/03_Forecasting_Maturity.py
+++ pages/03_Forecasting_Maturity.py
@@ -16,7 +16,7 @@ from utils.data_cleaning import clean_master_dataset
 
 st.set_page_config(
     page_title="Forecasting Maturity Analysis",
-    page_icon="🔮",
+    page_icon="📊",
     layout="wide"
 )
 
